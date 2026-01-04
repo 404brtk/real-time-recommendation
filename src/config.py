07c67b8658195d6458dc7df566aa8c883f591a21
404brtk <bzielonka1@proton.me>
2026-01-04 19:49:57 +0100

    feat(config): add basic configuration for data paths and services

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+# data paths
+DATA_DIR = Path("data")
+MODELS_DIR = DATA_DIR / "models"
+MAPPINGS_DIR = DATA_DIR / "mappings"
+PROCESSED_DIR = DATA_DIR / "processed"
+RAW_DIR = DATA_DIR / "raw"
+
+# redis
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+REDIS_USER_VECTOR_PREFIX = "user:vector:"
+
+# qdrant
+QDRANT_HOST = os.getenv("QDRANT_HOST", "localhost")
+QDRANT_PORT = int(os.getenv("QDRANT_PORT", 6333))
+QDRANT_COLLECTION_NAME = "items"
+VECTOR_SIZE = 32  # the same as in als rank
